a6ab754b8a171489e4afdb5d5e66049db9826bd4
muctebakarabey1 <muctebakarabey12@gmail.com>
2024-12-19 07:02:03 -0900

    version

--- src/postgresToHive.py
+++ src/postgresToHive.py
@@ -4,13 +4,13 @@ from pyspark.sql.functions import *
 spark = SparkSession.builder.master("local").appName("MiniProj").enableHiveSupport().getOrCreate()
 
 df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.132.73.146:5432/testdb") \
-    .option("driver", "org.postgresql.Driver").option("dbtable", "person") \
+    .option("driver", "org.postgresql.Driver").option("bigdata_nov_2024", "person") \
     .option("user", "consultants").option("password", "WelcomeItc@2022").load()
 df.printSchema()
 
 
 
-df.write.mode("overwrite").saveAsTable("mucteba.person")
+df.write.mode("overwrite").saveAsTable("bigdata_nov_2024.person")
 print("Successfully Load to Hive")
 
 # spark-submit --master local[*] --jars /var/lib/jenkins/workspace/nagaranipysparkdryrun/lib/postgresql-42.5.3.jar src/FirstLoadPostgressToHive.py
